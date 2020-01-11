22008a6f49262927db4b39277fcc46bb24fdccb7
ManuelCaballero <emanenxd@gmail.com>
2020-01-11 12:44:15 +0100

    Initial commit

---
+++ src/main/java/org/emanenxd/forge/mods/Main.java
@@ -0,0 +1,14 @@
+package org.emanenxd.forge.mods;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("MyTest")
+public class Main {
+	
+	private static final Logger LOGGER = LogManager.getLogger();
+	
+	//public 
+}
